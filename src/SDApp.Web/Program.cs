e34513780b11e44429a2fa20cf9d5d06f3fbd109
James Gunn <james@gunn.io>
2026-04-15 15:57:03 +0100

    Configure logging

--- src/SDApp.Web/Program.cs
+++ src/SDApp.Web/Program.cs
@@ -1,5 +1,25 @@
+using Serilog;
+using Serilog.Formatting.Compact;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSerilog((services, config) =>
+{
+    config.ReadFrom.Configuration(builder.Configuration);
+
+    if (builder.Environment.IsDevelopment())
+    {
+        config.WriteTo.Console();
+    }
+    else
+    {
+        config.WriteTo.Console(new CompactJsonFormatter());
+    }
+});
+
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 app.MapGet("/", () => "Hello World!");
 
