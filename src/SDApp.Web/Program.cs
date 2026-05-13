87ebbd7586599514d3a3c8ae26b2accefbb25873
James Gunn <james@gunn.io>
2026-05-13 19:21:20 +0100

    Add EFCore with Postgres

--- src/SDApp.Web/Program.cs
+++ src/SDApp.Web/Program.cs
@@ -1,3 +1,4 @@
+using SDApp.Web;
 using Serilog;
 using Serilog.Formatting.Compact;
 
@@ -17,9 +18,27 @@ builder.Services.AddSerilog((services, config) =>
     }
 });
 
+var postgresConnectionString = builder.Configuration.GetConnectionString(AppDbContext.ConnectionName) ??
+    throw new InvalidOperationException($"Connection string '{AppDbContext.ConnectionName}' not found.");
+
+builder.Services
+    .AddDbContext<AppDbContext>(
+        options => AppDbContext.Configure(options, postgresConnectionString),
+        contextLifetime: ServiceLifetime.Scoped,
+        optionsLifetime: ServiceLifetime.Singleton)
+    .AddDbContextFactory<AppDbContext>(
+        options => AppDbContext.Configure(options, postgresConnectionString),
+        lifetime: ServiceLifetime.Singleton);
+
 var app = builder.Build();
 
 app.UseSerilogRequestLogging();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
 
 app.MapGet("/health", () => "OK");
 
